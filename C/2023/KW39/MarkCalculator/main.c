67b59ea7bcead70eb60c46b9a4d5c432ff59a3ee
Florian <florian.loew@noseryoung.com>
2023-09-28 16:09:12 +0200

    added MarksCalculator

--- C/2023/KW39/MarkCalculator/main.c
+++ C/2023/KW39/MarkCalculator/main.c
@@ -3,16 +3,36 @@
 
 #define NUM_MARKS 5
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv)
+{
 
     printf("-----------------------\n");
     printf("Average Mark Calculator\n");
     printf("-----------------------\n");
-
-    /* 
+    float marks[NUM_MARKS];
+    float weights[NUM_MARKS];
+    float totalWeight;
+    float totalMarks;
+    /*
      * TODO - Read as many marks as defined in NUM_MARKS.
      *      - Save read marks in an array of length NUM_MARKS.
      */
+
+    for (int i = 0; i < NUM_MARKS; i++)
+    {
+        int fail = 0;
+        do
+        {
+            if (fail)
+            {
+                printf("ONLY NUMBERS BETWEEN 1 AND 6 ARE ALLOWED!!!\n");
+            }
+            printf("Enter Grade space Weight: ");
+            scanf("%f%f", &marks[i], &weights[i]);
+
+            fail = 1;
+        } while (marks[i] < 1.0 || marks[i] > 6.0);
+    }
 
     printf("-----------------------\n");
 
@@ -20,9 +40,16 @@ int main(int argc, char** argv) {
      * TODO - Calculate and store the sum of all marks.
      */
 
+    for (int i = 0; i < NUM_MARKS; i++)
+    {
+        totalMarks += weights[i] * marks[i];
+        totalWeight += weights[i];
+    }
+
     /*
      * TODO - Calculate and print the average mark.
      */
+    printf("The Average is %.2f\n\n", (totalMarks / totalWeight));
 
     /*
      * TODO - Extra tasks, implement only after completing all previous TODOs.
@@ -34,4 +61,3 @@ int main(int argc, char** argv) {
 
     return (EXIT_SUCCESS);
 }
-

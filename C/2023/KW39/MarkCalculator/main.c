cf633a6e0c2211ea24848a9561566a2e0a0362b6
Florian <florian.loew@noseryoung.com>
2023-09-28 14:32:18 +0200

    added template

---
+++ C/2023/KW39/MarkCalculator/main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define NUM_MARKS 5
+
+int main(int argc, char** argv) {
+
+    printf("-----------------------\n");
+    printf("Average Mark Calculator\n");
+    printf("-----------------------\n");
+
+    /* 
+     * TODO - Read as many marks as defined in NUM_MARKS.
+     *      - Save read marks in an array of length NUM_MARKS.
+     */
+
+    printf("-----------------------\n");
+
+    /*
+     * TODO - Calculate and store the sum of all marks.
+     */
+
+    /*
+     * TODO - Calculate and print the average mark.
+     */
+
+    /*
+     * TODO - Extra tasks, implement only after completing all previous TODOs.
+     *      - Round the calculated average to 2 decimal places (output only).
+     *      - While inputting, only allow values between 1.0 and 6.0.
+     *      - For every mark, input a weight and calculate the average accordingly.
+     *      - Allow for the manipulation of individual marks, using the index.
+     */
+
+    return (EXIT_SUCCESS);
+}
+

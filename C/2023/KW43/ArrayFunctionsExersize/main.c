7ca8f96b0d4d0e9a4b7bbf86340c8a6df88ea368
Florian <florian.loew@noseryoung.com>
2023-11-01 08:38:20 +0100

    added Array Function exersize

---
+++ C/2023/KW43/ArrayFunctionsExersize/main.c
@@ -0,0 +1,18 @@
+#define SIZE 100
+void initArray(int *array, int SIZE);
+void main(void)
+{
+    int number = 5;
+    int numberTwo = 2;
+    initArray(&number, SIZE);
+}
+void initArray(int *array, int size)
+{
+    int length = (sizeof(array) / sizeof(int));
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = 0;
+    }
+    array = 5;
+}
+numebrs = [ 3, 4, 5 ] & numbers[] : ajhdf | gze3bm gseg & number[7] = e

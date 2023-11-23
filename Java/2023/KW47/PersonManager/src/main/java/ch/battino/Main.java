062a4c6b6c3b24e61ed29dec2d1dd82503ad7026
Florian <florian.loew@noseryoung.com>
2023-11-23 11:14:29 +0100

    Added Person Manager

---
+++ Java/2023/KW47/PersonManager/src/main/java/ch/battino/Main.java
@@ -0,0 +1,25 @@
+package ch.battino;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("==============================================");
+        System.out.println("WELCOME TO THE PERSON MANAGER FROM THE FUTUER:");
+        System.out.println("==============================================");
+        ArrayList<Person> personList= new ArrayList<Person>();
+
+        do {
+            personList.add(new Person());
+            System.out.print(personList.getLast().toPrintable());
+            System.out.println("Do you want to Add another Person? y/n");
+            Scanner sc = new Scanner(System.in);
+            String repeatPrompt = sc.nextLine();
+
+            if (!repeatPrompt.toLowerCase().equals("y")){
+                break;
+            }
+        }while (true);
+    }
+}
---
+++ Java/2023/KW47/PersonManager/src/main/java/ch/battino/Person.java
@@ -0,0 +1,68 @@
+package ch.battino;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Person {
+    private String name = "your person's";
+    private int age;
+    private String originCountry;
+    private ArrayList<String> favoriteFoods;
+    private static final Scanner scan = new Scanner(System.in);
+
+
+
+    public  Person(){
+        allInput();
+    }
+
+    public void allInput(){
+this.name = inputStr("Name:");
+this.age = inputInt("Age:");
+this.originCountry = inputStr("Country of Origin:");
+String inputFavoriteFoods = inputStr("Favorite Foods Separated by a \";\"");
+this.favoriteFoods = inputSeperator(inputFavoriteFoods);
+    }
+    public int inputInt(String inputAsked){
+        System.out.println("Enter "+ this.name+"'s "+inputAsked);
+        do {
+        try {
+            int temp = scan.nextInt();
+            scan.nextLine();
+            return temp;
+        }catch (Exception ignored){
+            System.out.println("ENTER A NUMBER!!!");
+            scan.nextLine();
+        }
+        }while (true);
+    }
+    public String inputStr(String inputAsked){
+        System.out.println("Enter "+ this.name+"'s "+inputAsked);
+        return scan.nextLine();
+    }
+    private ArrayList<String> inputSeperator(String sentence){
+        String[] array = sentence.replaceAll("[^a-zA-Z0-9;]", "").split(";");
+        ArrayList<String> favFoods = new ArrayList<String>();
+        favFoods.addAll(Arrays.asList(array));
+        return favFoods;
+    }
+
+public String arraylistToString(ArrayList<String> favoriteFoods){
+StringBuilder temp = new StringBuilder();
+        for (String i:favoriteFoods){
+temp.append("\n");
+        }
+        return temp.toString();
+}
+
+    public String toPrintable() {
+        return "name:" + name + '\n' +
+                "age:" + age + '\n'+
+                "originCountry:'" + originCountry + '\n' +
+                "favoriteFoods:" + '\n' + favoriteFoods + '\n';
+    }
+    public void closeInput() {
+        scan.close();
+    }
+}

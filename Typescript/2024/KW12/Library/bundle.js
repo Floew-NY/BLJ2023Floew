fbaee08f081ad46c4d247e286d616e9f8271b12a
Florian <florian.loew@noseryoung.com>
2024-03-28 09:05:04 +0100

    Add mediaLibrary Typescript

---
+++ Typescript/2024/KW12/Library/bundle.js
@@ -0,0 +1,9 @@
+// index.ts
+var mediumList = [];
+mediumList.push({ name: "The Hobbit", publisher: "George Allen & Unwin", copyRightHolder: "J.R.R. Tolkien", pages: 310 });
+mediumList.push({ name: "The Lord of the Rings", publisher: "George Allen & Unwin", copyRightHolder: "J.R.R. Tolkien", pages: 1500 });
+mediumList.push({ name: "The Shawshank Redemption", publisher: "Columbia Pictures", copyRightHolder: "Castle Rock Entertainment", duration: 142, mainCharacter: "Andy Dufresne" });
+mediumList.push({ name: "The Dark Knight", publisher: "Warner Bros. Pictures", copyRightHolder: "DC Comics", duration: 152, mainCharacter: "Batman" });
+mediumList.push({ name: "The Legend of Zelda: Breath of the Wild", publisher: "Nintendo", copyRightHolder: "Nintendo", size: 13, developer: "Nintendo", genre: "Action-adventure" });
+mediumList.push({ name: "Super Mario Odyssey", publisher: "Nintendo", copyRightHolder: "Nintendo", size: 5, developer: "Nintendo", genre: "Platformer" });
+console.log(mediumList);
--- Typescript/2024/KW12/Library/index.ts
+++ Typescript/2024/KW12/Library/index.ts
@@ -52,3 +52,16 @@ class VideoGame extends Medium {
     this.genre = genre;
   }
 }
+
+
+
+let mediumList: Medium[] = [];
+
+mediumList.push({name: 'The Hobbit', publisher: 'George Allen & Unwin', copyRightHolder: 'J.R.R. Tolkien', pages: 310} as Book);
+mediumList.push({name: 'The Lord of the Rings', publisher: 'George Allen & Unwin', copyRightHolder: 'J.R.R. Tolkien', pages: 1500} as Book);
+mediumList.push({name: 'The Shawshank Redemption', publisher: 'Columbia Pictures', copyRightHolder: 'Castle Rock Entertainment', duration: 142, mainCharacter: 'Andy Dufresne'} as Movie);
+mediumList.push({name: 'The Dark Knight', publisher: 'Warner Bros. Pictures', copyRightHolder: 'DC Comics', duration: 152, mainCharacter: 'Batman'} as Movie);
+mediumList.push({name: 'The Legend of Zelda: Breath of the Wild', publisher: 'Nintendo', copyRightHolder: 'Nintendo', size: 13, developer: 'Nintendo', genre: 'Action-adventure'} as VideoGame);
+mediumList.push({name: 'Super Mario Odyssey', publisher: 'Nintendo', copyRightHolder: 'Nintendo', size: 5, developer: 'Nintendo', genre: 'Platformer'} as VideoGame);
+
+console.log(mediumList);

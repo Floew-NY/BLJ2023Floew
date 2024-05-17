02c1d79acfd10631c4eb4071f39051a5ce9a7dbb
Battino <battino46@gmail.com>
2024-05-17 16:38:05 +0200

    add tcp server and reciever

--- rust/2024/KW20/friends_simulate/src/main.rs
+++ rust/2024/KW20/friends_simulate/src/main.rs
@@ -1,5 +1,29 @@
+use std::io::{stdout, Read, Result, Write};
 use std::net::{TcpListener, TcpStream};
+use std::thread::sleep;
 
-fn main() {
-    println!("Hello, world!");
+fn handle_client(mut stream: TcpStream) -> Result<()> {
+    println!("{}", stream.peer_addr()?);
+    let mut message: Vec<u8> = vec![];
+    loop {
+        let mut buff = [0; 1];
+        if stream.read(&mut buff)? > 0 {
+            message.push(buff[0]);
+        }
+        if buff[0] == b'\n' {
+            print!(
+                "message: {}",
+                String::from_utf8(message.clone()).expect("not utf8")
+            );
+            message.clear();
+        }
+    }
+}
+fn main() -> Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:0")?;
+    println!("{}", listener.local_addr()?);
+    for stream in listener.incoming() {
+        handle_client(stream?)?;
+    }
+    Ok(())
 }
--- rust/2024/KW20/simulate_friends/src/main.rs
+++ rust/2024/KW20/simulate_friends/src/main.rs
@@ -1,3 +1,39 @@
-fn main() {
-    println!("Hello, world!");
+use std::{
+    io::{stdin, stdout, Result, Write},
+    net::TcpStream,
+};
+
+fn main() -> Result<()> {
+    print!("Please enter the address: ");
+    stdout().flush()?;
+
+    let input_addr = user_console_input();
+
+    println!("You're connecting to: {}", input_addr);
+
+    let mut stream = TcpStream::connect(input_addr)?;
+    loop {
+        let mut message = user_console_input();
+        message.push('\n');
+        if message.is_empty() {
+            break;
+        }
+        stream.write(message.as_bytes())?;
+        stream.flush()?;
+    }
+    Ok(())
+}
+
+fn user_console_input() -> String {
+    let mut input = String::new();
+    stdin()
+        .read_line(&mut input)
+        .expect("Did not enter a correct string");
+    if let Some('\n') = input.chars().next_back() {
+        input.pop();
+    }
+    if let Some('\r') = input.chars().next_back() {
+        input.pop();
+    }
+    input
 }

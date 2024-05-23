cee50aa8a52053efcf2decf581682f60069c6947
Florian <florian.loew@noseryoung.com>
2024-05-23 16:50:11 +0200

    make 2 way communication possible

--- rust/2024/KW20/friends_simulate/src/main.rs
+++ rust/2024/KW20/friends_simulate/src/main.rs
@@ -1,22 +1,34 @@
-use std::io::{stdout, Read, Result, Write};
+use std::io::{stdin, stdout, Read, Result, Write};
 use std::net::{TcpListener, TcpStream};
-use std::thread::sleep;
+use std::thread;
 
 fn handle_client(mut stream: TcpStream) -> Result<()> {
-    println!("{}", stream.peer_addr()?);
-    let mut message: Vec<u8> = vec![];
+    let mut responce_stream = stream.try_clone()?;
+    let thread_handler = thread::spawn(move || -> Result<()> {
+        let mut message: Vec<u8> = vec![];
+        loop {
+            let mut buff = [0; 1];
+            if stream.read(&mut buff)? > 0 {
+                message.push(buff[0]);
+            }
+            if buff[0] == b'\n' {
+                print!(
+                    "message: {}",
+                    String::from_utf8(message.clone()).expect("not utf8")
+                );
+                message.clear();
+            }
+        }
+    });
+    println!("{}", responce_stream.peer_addr()?);
     loop {
-        let mut buff = [0; 1];
-        if stream.read(&mut buff)? > 0 {
-            message.push(buff[0]);
+        let mut message = user_console_input();
+        message.push('\n');
+        if message.is_empty() {
+            break Ok(());
         }
-        if buff[0] == b'\n' {
-            print!(
-                "message: {}",
-                String::from_utf8(message.clone()).expect("not utf8")
-            );
-            message.clear();
-        }
+        responce_stream.write(message.as_bytes())?;
+        responce_stream.flush()?;
     }
 }
 fn main() -> Result<()> {
@@ -26,4 +38,18 @@ fn main() -> Result<()> {
         handle_client(stream?)?;
     }
     Ok(())
+}
+
+fn user_console_input() -> String {
+    let mut input = String::new();
+    stdin()
+        .read_line(&mut input)
+        .expect("Did not enter a correct string");
+    if let Some('\n') = input.chars().next_back() {
+        input.pop();
+    }
+    if let Some('\r') = input.chars().next_back() {
+        input.pop();
+    }
+    input
 }
--- rust/2024/KW20/simulate_friends/src/main.rs
+++ rust/2024/KW20/simulate_friends/src/main.rs
@@ -1,6 +1,7 @@
 use std::{
-    io::{stdin, stdout, Result, Write},
+    io::{stdin, stdout, Read, Result, Write},
     net::TcpStream,
+    thread,
 };
 
 fn main() -> Result<()> {
@@ -12,10 +13,28 @@ fn main() -> Result<()> {
     println!("You're connecting to: {}", input_addr);
 
     let mut stream = TcpStream::connect(input_addr)?;
+    let mut responceStream = stream.try_clone()?;
+    let thread_handle = thread::spawn(move || -> Result<()> {
+        let mut message: Vec<u8> = vec![];
+        loop {
+            let mut buff = [0; 1];
+            if responceStream.read(&mut buff)? > 0 {
+                message.push(buff[0]);
+            }
+            if buff[0] == b'\n' {
+                print!(
+                    "message: {}",
+                    String::from_utf8(message.clone()).expect("not utf8")
+                );
+                message.clear();
+            }
+        }
+    });
     loop {
         let mut message = user_console_input();
         message.push('\n');
         if message.is_empty() {
+            let _ = thread_handle.join().expect("coulnt join");
             break;
         }
         stream.write(message.as_bytes())?;
